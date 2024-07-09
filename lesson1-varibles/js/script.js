dee660be87afce84d670dcefa575a646474791d1
Oleksii Slavych <oleksii.slavych@devcom.com>
2024-07-09 16:00:48 +0300

    Initial commit, added lesson 1

---
+++ lesson1-varibles/js/script.js
@@ -0,0 +1,71 @@
+"use strict";
+//Ініціалізація змінних
+// var x = 3;
+// var y = 5;
+// var z = x + y;
+
+// let x = 2;
+// let y = 3;
+// console.log(y);
+// let z = x + y;
+// console.log(z);
+// y = 5;
+// console.log(y);
+
+// const x = 3;
+// x = 5;
+
+// let x;
+// let y;
+// x = 5;
+// y = 3;
+// let z = x + y;
+
+// console.log(z);
+
+//Типи даних
+
+// let age = 24;
+// let userName = "Oleksiy";
+// let isAuthorized = true;
+// let unknown = null;
+
+// console.log("some number", 5);
+// console.log("user name", userName);
+// console.log("is user authorized", isAuthorized);
+// console.log(unknown);
+
+//Оператор typeof
+
+// console.log(typeof age);
+// console.log(typeof userName);
+// console.log(typeof isAuthorized);
+// console.log(typeof unknown);
+
+//Взаємодія з користувачем
+
+// alert("Permission denied");
+
+//const isUserAccepted = confirm("please accept cookies");
+
+// console.log("користувач натиснув", isUserAccepted);
+// const userData = prompt("set your data");
+
+// console.log("користувач написав", userData);
+
+// if (isUserAccepted) {
+//   console.log("ми попали в блок if");
+// } else {
+//   console.log("ми попали в блок else");
+// }
+
+// console.log("код виконався");
+
+// const message = isUserAccepted
+//   ? console.log("ми попали в блок if")
+//   : console.log("ми попали в блок else");
+// const userData = prompt("set your age");
+// const isAdult = userData > 18 ? "user is adult" : "user is not adult";
+
+// console.log(userData);
+// console.log(typeof userData);
