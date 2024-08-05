d575cbf8d5bf14bde5e9f8571bbafd537da2ca97
Oleksii Slavych <oleksii.slavych@devcom.com>
2024-08-05 20:38:12 +0300

    func uploaded

---
+++ lesson9-functions/js/script.js
@@ -0,0 +1,87 @@
+function greeting() {
+  console.log("Hello world");
+}
+// greeting();
+
+const greeting2 = function () {
+  console.log("Hello world");
+};
+
+greeting2();
+
+function sum(a = 0, b = 0, c = 0) {
+  const result = a + b + c;
+  return result;
+  const result2 = a * b * c;
+  console.log(result2);
+}
+
+const funcResult = sum(15, 12, 8);
+console.log(funcResult);
+
+function checkUserRole(role) {
+  let result;
+  if (role === "admin") {
+    result = "У вас всі права!";
+    return result;
+  }
+  if (role === "moderator") {
+    result = "Ви можете міняти контент";
+    return result;
+  }
+  if (role === "user") {
+    result = "Ви можете переглядати вміст сайду";
+    return result;
+  }
+
+  return "Невідомий користувач";
+  // if (role === "admin") {
+  //   console.log("У вас всі права!");
+  // } else if (role === "moderator") {
+  //   console.log("Ви можете міняти контент");
+  // } else if (role === "user") {
+  //   console.log("Ви можете переглядати вміст сайду");
+  // } else {
+  //   console.log("Невідома роль");
+  // }
+}
+
+console.log(checkUserRole("moderator"));
+
+console.log(sum(15, undefined, 25));
+
+hoistFunction();
+
+// console.log(test);
+
+// const test = 5;
+
+function hoistFunction() {
+  console.log("Я була викликана");
+}
+
+//testFunc();
+
+const testFunc = function () {
+  console.log("Я також була викликаною");
+};
+
+const sum2 = function (b, a, ...args) {
+  let result = 0;
+  args.splice(0, 0, 14, 51, 61, 13);
+  for (const argument of args) {
+    result += argument;
+  }
+  console.log(a, b);
+  return result;
+};
+
+const log = function (...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(`Елемент ${i + 1}: ${args[i]}`);
+  }
+};
+
+log(15, true, "Олексій", "Микола");
+const array = [15, true, "Олексій", "Микола"];
+log(...array);
