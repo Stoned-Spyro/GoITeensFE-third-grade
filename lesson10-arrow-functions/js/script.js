58a62eb1f96ccd8b1e131742d03ca5a7011a2739
Oleksii Slavych <oleksii.slavych@devcom.com>
2024-08-12 21:54:58 +0300

    arrow func 2

--- lesson10-arrow-functions/js/script.js
+++ lesson10-arrow-functions/js/script.js
@@ -3,7 +3,6 @@ const repeat = function (n, callback) {
     callback(i);
   }
 };
-
 const createIterationMessage = function (optionMessage = "") {
   const message = optionMessage;
   console.log(message);
@@ -22,7 +21,6 @@ console.log("-------------------------------------------------");
 repeat(15, prettyLogValue);
 console.log("--------------------------------------------------");
 repeat(8, pow);
-
 const filter = function (array, test) {
   let resultArray = [];
   for (const el of array) {
@@ -63,3 +61,87 @@ const filteredNumbers = filter(numbersArray, (el) => {
 console.log(filteredNumbers);
 const filteredEvenNumbers = filter(numbersArray, (el) => el % 2 === 0);
 console.log(filteredEvenNumbers);
+// const test = function() {
+//   console.log("hello world");
+// }
+
+//let testVar = 10;
+// console.log(innerVar);
+
+// for (let index = 0; index < 10; index++) {
+//   var innerVar = 10;
+// }
+// const test = () => {
+//   const innerTest = function () {
+//     console.log("hello from inner");
+//   };
+//   innerTest();
+// };
+
+// test();
+
+const logger = (name) => {
+  console.log("Hello,", name);
+};
+
+logger("Oleksii");
+console.log(logger);
+
+const numberGenerator = () => {
+  const result = Math.floor(Math.random() * 10 + 1);
+  const message = "Випадкове число: " + result;
+  return message;
+};
+
+const myRandomNumber = numberGenerator();
+console.log(myRandomNumber);
+
+// const funcWithParameters = (...args) => {
+//   for (const arg of args) {
+//     console.log(arg);
+//   }
+// };
+
+const funcWithParameters = function (...args) {
+  let result = 0;
+  for (const arg of arguments) {
+    result += arg;
+  }
+  return result;
+};
+
+const arr = [15, 2, 4, 8];
+
+// for (let num of arr) {
+//   num = num * 2;
+//   console.log(num);
+// }
+
+console.log(arr);
+//console.log(funcWithParameters(10, 15, 25, 40, 100, 7, 2, 1));
+
+// const stack = () => {
+//   function innerStack() {
+//     console.log(unknown);
+//   }
+//   innerStack();
+// };
+
+// stack();
+
+const bar = function () {
+  console.log("bar");
+};
+
+const baz = function () {
+  console.log("baz");
+};
+
+const foo = function () {
+  console.log("foo");
+  bar();
+  baz();
+  console.log("End");
+};
+const varti = 10;
+foo();
