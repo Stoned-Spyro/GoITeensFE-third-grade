74e86ab51233c68878e51b8909f6c22e089e70b7
Oleksii Slavych <oleksii.slavych@devcom.com>
2024-08-27 14:25:08 +0300

    objects first part

---
+++ lesson11-objects/js/script.js
@@ -0,0 +1,20 @@
+const nazar = {
+  email: "nazar@gmail.com",
+  age: 16,
+  gender: "male",
+  surname: "Руденко",
+};
+
+console.log(nazar);
+console.log(nazar.job);
+console.log(nazar);
+
+delete nazar.surname;
+
+nazar.age = 17;
+
+const address = 'Lviv'
+
+// nazar.address = "Lviv";
+
+console.log(nazar);
